ebf28cb286ce4b3c6d34db504d2a0933721e8f78
daswosau <daswosau@gmail.com>
2026-09-03 12:35:51 +0200

    Added java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        1.
-        2.
-
         DamnBigNumber num1 = new DamnBigNumber("123");
         DamnBigNumber num2 = new DamnBigNumber("456");
         DamnBigNumber result = DamnBigNumbers.add(num1, num2);
